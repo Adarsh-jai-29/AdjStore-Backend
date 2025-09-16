129905aee7c35cf61248590a1197a1e038be0c1f
Adarsh <adarshjai29@gmail.com>
2025-09-16 23:00:32 +0530

    fix: CORS issue😉

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001
 
 
 app.use(cors({
-  origin: 'http://localhost:5173' || 'https://adj-store.vercel.app/', 
+  origin: ["http://localhost:5173", "https://adj-store.vercel.app"], 
   credentials: true,
 }));
 
